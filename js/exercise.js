057d08ea1695fcb5e561c49f4e0052f3044d9fcf
duyminh <hse.duyminh@gmail.com>
2023-11-23 07:50:31 +0700

    Exercise

---
+++ js/exercise.js
@@ -0,0 +1,61 @@
+// var toan = 8;
+// var sinh = 9;
+// var ly = 9;
+// var avange = (toan + sinh + ly) / 3;
+// var sum = toan + ly + sinh;
+// console.log("avenger: ", avange);
+// console.log("sum: ", sum);
+
+// Điểm trung bình và tổng điểm
+
+// var celsius = 0;
+// var fahrenheit = (celsius / 5) * 9 + 32;
+// console.log("fahrenheit: ", fahrenheit);
+
+// Giá trị của 0 độ C
+
+// var radius = 3;
+// var pi = 3.14;
+// var cicle_acreage = radius * radius * pi;
+// var perimeter = 2 * radius * pi
+
+// console.log("cicle_acreage: ", cicle_acreage);
+// console.log("perimeter: ", perimeter);
+
+// Tính giá trị chu vi và bán kính
+
+// var chemistry = +prompt("Mời bạn nhập điểm môn hoá: ");
+// var math = +prompt("Mời bạn nhập điểm môn toan: ");
+// var physics = +prompt("Mời bạn nhập điểm môn ly: ");
+// var average = (chemistry + math + physics) / 3;
+// console.log("rerult: ", average);
+
+//Model xác nhận từ người dùng
+// var isLogout = window.comfirm("Bạn có muốn đăng xuất không?");
+// if (isLogout === true) {
+//     console.log("Đăng xuất thành công")
+// } else {
+//     console.log("Bạn đã đăng xuất!");
+// }
+// Dạng thông báo (Alert)
+// window.alert("Phiên của bạn đã hết.");
+
+// Yêu cầu người nhập vào tuổi, nếu tuổi lớn hơn 18 và là con trai thì chỉ hiển thị thông báo là đủ điều kiện
+// Không đủ điều kiện
+
+// var myAge = +prompt("Mời bạn nhập tuổi: ");
+// var gender = prompt("Mời bạn nhập giới tính");
+// if (myAge >= 18 && gender === "Nam") {
+//     alert("Bạn đã đủ tuổi!");
+// } else {
+//     alert("Bạn chưa đủ tuổi");
+// }
+// Những giá trị nhận là false trong Js
+// Một chuỗi rỗng, false, null, 0, NaN, undefined
+
+var value = false;
+if (value) {
+    console.log("Điều kiện đúng");
+} else {
+    console.log("Điều kiện sai");
+}
